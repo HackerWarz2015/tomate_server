7df552677a64f918c59d418c0c6ea6ed4e8330c6
shiftky <shiftky@gmail.com>
2015-06-07 02:05:14 +0900

    Modify validation of Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ActiveRecord::Base
   belongs_to :from_user, class_name: User
   belongs_to :to_user, class_name: User
+
+  validates :from_user, presence: true, uniqueness: { scope: :from_user_id }
+  validates :to_user, presence: true, uniqueness: { scope: :to_user_id }
 end
+
