202f6acf8fe9c211b8c3fba5ecd79836b14c40ce
shiftky <shiftky@gmail.com>
2015-06-07 01:59:48 +0900

    Add Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   has_many :user_tasks
   has_many :users_rewards, dependent: :destroy
   has_many :rewards, through: :users_rewards
+  has_many :friendship_of_from_user, class_name: Friendship, foreign_key: :from_user_id, dependent: :destroy
+  has_many :friendship_of_to_user, class_name: Friendship, foreign_key: :to_user_id, dependent: :destroy
+  has_many :friends_of_from_user, through: :friendship_of_from_user, source: :to_user
+  has_many :friends_of_to_user, through: :friendship_of_to_user, source: :from_user
 
   validates :authentication_token, uniqueness: true, allow_nil: true
 
