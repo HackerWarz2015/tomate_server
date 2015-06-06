ec0a3a6680a75c326a42f9dbc449c8e1a6681975
shiftky <shiftky@gmail.com>
2015-06-06 18:14:23 +0900

    Add UserTask model

---
+++ db/migrate/20150606091241_create_user_tasks.rb
@@ -0,0 +1,15 @@
+class CreateUserTasks < ActiveRecord::Migration
+  def change
+    create_table :user_tasks do |t|
+      t.integer :user_id
+      t.string :title
+      t.integer :difficulty
+      t.integer :steps
+      t.must :is
+      t.datetime :failure_at
+      t.datetime :done_at
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/user_tasks.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :user_task do
+    user_id 1
+title "MyString"
+difficulty 1
+steps 1
+is ""
+failure_at "2015-06-06 18:12:41"
+done_at "2015-06-06 18:12:41"
+  end
+
+end
