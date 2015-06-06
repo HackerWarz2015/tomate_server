c5a7e563b386e5f3ad9b6b3ee2f91dadbe14000d
shiftky <shiftky@gmail.com>
2015-06-06 17:55:25 +0900

    Add sample response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApiController
+  def index
+    @users = {
+      users: [
+        {
+          id: 1,
+          name: "ks",
+          email: "ks@ks.com",
+          steps: 10000
+        }
+      ]
+    }
+    render json: @users
+  end
 end
